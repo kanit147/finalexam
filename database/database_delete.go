f3bb180941d1eff121917c4ec134a1904a9ace4f
l3oong <l3oong@l3oongs-MacBook.local>
2020-06-20 19:16:20 +0700

    refactor delete function

---
+++ database/database_delete.go
@@ -0,0 +1,15 @@
+package database
+
+import "fmt"
+
+func DeleteById(id int) error {
+	stmt, err := Con().Prepare("DELETE FROM customers WHERE id = $1")
+	if err != nil {
+		return fmt.Errorf("can't prepare delete statement: %w", err)
+	}
+
+	if _, err := stmt.Exec(id); err != nil {
+		return fmt.Errorf("can't execute delete statment: %w", err)
+	}
+	return nil
+}
